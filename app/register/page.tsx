4b0c9f89657e285533138bbd8c3ce9d11a579f84
Stagiaire XERFI <mathis.bc23@gmail.com>
2025-05-27 12:02:45 +0200

    changement dir

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "../../components/ui/button";
-import { Input } from "../../components/ui/input";
-import { Alert } from "../../components/ui/alert";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Alert } from "../components/ui/alert";
 import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
